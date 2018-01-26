42f17e819cd85f2633b3a0373b4f0493a87f2e25
Yuki Hirai <yukihirai0505@gmail.com>
2018-01-26 10:47:25 +0900

    Update script

--- cryptoCurrency/getZaifKuCoinCryptopiaCryptoCurrencyData.js
+++ cryptoCurrency/getZaifKuCoinCryptopiaCryptoCurrencyData.js
@@ -1,5 +1,6 @@
 var BK = SpreadsheetApp.getActiveSpreadsheet(),
-  SHEET = BK.getSheetByName('portfolio');
+  SHEET = BK.getSheetByName('portfolio'),
+  QUERY_STRING = Math.random();
 
 function onOpen() {
   function showMenu() {
@@ -12,20 +13,20 @@ function onOpen() {
   showMenu();
 }
 
+function fetchJson(url) {
+  return JSON.parse(UrlFetchApp.fetch(url));
+}
 
 function setData() {
-  function fetchJson(url) {
-    return JSON.parse(UrlFetchApp.fetch(url));
-  }
-
   var btcJpyPrice = fetchJson('https://api.zaif.jp/api/1/last_price/btc_jpy').last_price,
     kuCoinPrices = fetchJson('https://api.kucoin.com/v1/open/tick').data,
     cryptopiaPrices = fetchJson('https://www.cryptopia.co.nz/api/GetMarkets').Data,
-    range = SHEET.getRange(2, 1, SHEET.getLastRow(), 5),
+    range = SHEET.getRange(2, 1, SHEET.getLastRow(), 8),
     data = range.getValues().map(function (e) {
       var symbol = e[0],
         place = e[1],
-        quality = e[2];
+        quality = e[2],
+        getBtcPrice = e[3];
       Logger.log(symbol);
       if (symbol) {
         var price;
@@ -46,11 +47,44 @@ function setData() {
         if (price) {
           var jpy = price * btcJpyPrice;
           Logger.log(jpy);
-          e[3] = jpy;
-          e[4] = jpy * quality;
+          e[4] = price;
+          e[5] = (price / getBtcPrice) - 1;
+          e[6] = jpy;
+          e[7] = jpy * quality;
         }
       }
       return e;
     });
   range.setValues(data);
+  setResultColor();
+  getMarketCap();
+}
+
+function getMarketCap() {
+  var range = SHEET.getRange(2, 1, SHEET.getLastRow(), 9),
+    marketCap = fetchJson('https://api.coinmarketcap.com/v1/ticker/?' + QUERY_STRING + '&limit=0'),
+    data = range.getValues().map(function (e) {
+      var symbol = e[0];
+      Logger.log(symbol);
+      if (symbol) {
+        e[8] = marketCap.filter(function (e) {
+          if (e.symbol === symbol) {
+            return e;
+          }
+        })[0].rank;
+      }
+      return e;
+    });
+  range.setValues(data);
+}
+
+function setResultColor() {
+  for (var i = 2; i <= SHEET.getLastRow(); i++) {
+    var range = SHEET.getRange(i, 6);
+    if (SHEET.getRange(i, 6).getValue() > 0) {
+      range.setBackground('#DFF2BF');
+    } else {
+      range.setBackground('#FFBABA');
+    }
+  }
 }
