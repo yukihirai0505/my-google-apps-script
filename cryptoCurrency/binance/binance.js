f7cdda6caf8b422e659f4d45713c0bad7ef37bdd
Yuki Hirai <yukihirai0505@gmail.com>
2018-04-08 23:24:41 +0900

    Update scripts

--- cryptoCurrency/binance/binance.js
+++ cryptoCurrency/binance/binance.js
@@ -1,7 +1,12 @@
 var BK = SpreadsheetApp.getActiveSpreadsheet(),
   SHEETS = BK.getSheets(),
   BINANCE_API_URL = 'https://api.binance.com',
-  BTC_SYMBOL = 'BTC';
+  BTC_SYMBOL = 'BTC',
+  SYMBOLS = SHEETS[0].getRange(2, 1, SHEETS[0].getLastRow(), 1).getValues().filter(function (e) {
+    if (e[0]) {
+      return e;
+    }
+  });
 
 function fetchJson(url) {
   return JSON.parse(UrlFetchApp.fetch(url));
@@ -21,47 +26,54 @@ function setSymbols() {
   });
 }
 
+function getKLines(symbol, times) {
+  Utilities.sleep(500);
+  var url = BINANCE_API_URL + '/api/v1/klines?symbol=' + symbol + BTC_SYMBOL + '&interval=1d';
+  url = times ? url + times : url;
+  return fetchJson(url).map(function (e) {
+    var openTime = e[0],
+      openPrice = e[1],
+      highPrice = e[2],
+      lowPrice = e[3],
+      closePrice = e[4],
+      volume = e[5],
+      closeTime = e[6],
+      quoteAssetVolume = e[7],
+      numberOfTrades = e[8],
+      takerBuyBaseAssetVol = e[9],
+      takerBuyQuoteAssetVol = e[10],
+      ignore = e[11];
+    return {
+      openTime: new Date(openTime),
+      highPrice: highPrice,
+      lowPrice: lowPrice,
+      volume: volume
+    };
+  });
+}
+
+function setTwoDaysData() {
+  var today = new Date(),
+    yesterday = new Date();
+  yesterday.setDate(yesterday.getDate() - 1);
+  yesterday.setHours(0, 0, 0, 0);
+  var times = '&startTime=' + yesterday.getTime() + '&endTime=' + today.getTime();
+  SYMBOLS.map(function (e, i) {
+    var data = getKLines(e[0], times);
+    Logger.log(data);
+  });
+}
+
 function setPastData() {
   var filterDateTime = new Date(2018, 0, 1).getTime();
 
-  function getKLines(symbol) {
-    Utilities.sleep(500);
-    var kLines = fetchJson(BINANCE_API_URL + '/api/v1/klines?symbol=' + symbol + BTC_SYMBOL + '&interval=1d').map(function (e) {
-      var openTime = e[0],
-        openPrice = e[1],
-        highPrice = e[2],
-        lowPrice = e[3],
-        closePrice = e[4],
-        volume = e[5],
-        closeTime = e[6],
-        quoteAssetVolume = e[7],
-        numberOfTrades = e[8],
-        takerBuyBaseAssetVol = e[9],
-        takerBuyQuoteAssetVol = e[10],
-        ignore = e[11];
-      return {
-        openTime: new Date(openTime),
-        highPrice: highPrice,
-        lowPrice: lowPrice,
-        volume: volume
-      };
-    }).filter(function (e) {
+  var dateColumns;
+  SYMBOLS.forEach(function (e, i) {
+    var data = getKLines(e[0]).filter(function (e) {
       if (e.openTime.getTime() > filterDateTime) {
         return e;
       }
     });
-    return kLines;
-  }
-
-  var symbols = SHEETS[0].getRange(2, 1, SHEETS[0].getLastRow(), 1).getValues().filter(function (e) {
-      if (e[0]) {
-        return e;
-      }
-    }),
-    dateColumns;
-  Logger.log(symbols);
-  symbols.forEach(function (e, i) {
-    var data = getKLines(e[0]);
     var dates = data.map(function (e) {
       return e.openTime;
     });
