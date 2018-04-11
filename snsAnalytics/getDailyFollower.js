c492307f33846330066ccb1084b64aff4409a321
Yuki Hirai <yukihirai0505@gmail.com>
2018-04-11 17:41:29 +0900

    Add Twitter Daily Follower Count

--- snsAnalytics/getDailyFollower.js
+++ snsAnalytics/getDailyFollower.js
@@ -1,12 +1,58 @@
-// https://www.youtube.com/channel/UClcWgUnEr79fJBcpqnm4M1w?view_as=subscriber
-
-// 各SNSのフォロワー数を定期的に更新していく
-// アカウント名/チャンネルID, SNS, 日付....
-
 var BK = SpreadsheetApp.getActiveSpreadsheet(),
   TWITTER_SHEET = BK.getSheetByName('Twitter'),
   YOUTUBE_SHEET = BK.getSheetByName('Youtube');
 
-// Twitter API(Get users so check screen name equals the query)
-// https://script.google.com/macros/s/AKfycbwiFrBiaQcCbk2FqV8S2yuteQTml3GZgsXNLW237_YE7B3NV4Q/exec?q=yabaiwebyasan
+function fetchJson(url) {
+  return JSON.parse(UrlFetchApp.fetch(url));
+}
 
+function setTwitterAccountInfo() {
+
+}
+
+function setDailyTwitterFollowerData() {
+  var today = new Date(),
+    dateStartColumnNum = 5,
+    accountStartRow = 2,
+    dateValues = TWITTER_SHEET.getRange(1, dateStartColumnNum, 1, TWITTER_SHEET.getLastColumn()).getValues().filter(function (e) {
+      if (e[0]) {
+        return e;
+      }
+    })[0],
+    accountNames = TWITTER_SHEET.getRange(accountStartRow, 1, TWITTER_SHEET.getLastRow(), 1).getValues().filter(function (e) {
+      if (e[0]) {
+        return e;
+      }
+    });
+  accountNames.map(function (accountNameData, accountNameIndex) {
+    var dateColumnNum,
+      accountName = accountNameData[0];
+    if (dateValues) {
+      dateValues.forEach(function (dateVal, dateIndex) {
+        if (dateFormat(new Date(dateVal)) === dateFormat(today)) {
+          dateColumnNum = dateIndex + dateStartColumnNum;
+        }
+      });
+    }
+    if (!dateColumnNum) {
+      // If there is no specified date column, set new date column
+      dateColumnNum = dateValues ? dateValues.length + 1 : dateStartColumnNum;
+      if (accountNameIndex === 0) {
+        TWITTER_SHEET.getRange(1, dateColumnNum).setValue(today);
+      }
+    }
+    var twitterAPIUrl = 'https://script.google.com/macros/s/AKfycbwiFrBiaQcCbk2FqV8S2yuteQTml3GZgsXNLW237_YE7B3NV4Q/exec?q=' + accountName;
+    var accountData = fetchJson(twitterAPIUrl).filter(function (accountInfo) {
+      if (accountInfo.screen_name === accountName) {
+        return accountInfo;
+      }
+    })[0];
+    var followerCount = accountData ? accountData.followers_count : 'not found';
+    TWITTER_SHEET.getRange(accountNameIndex + accountStartRow, dateColumnNum).setValue(followerCount);
+  });
+}
+
+function dateFormat(date) {
+  var formatType = 'yyyy/MM/dd';
+  return Utilities.formatDate(date, 'JST', formatType);
+}
