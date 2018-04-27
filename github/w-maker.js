3f8af9d53f9ef1702690479289e32b861b9a2e6f
Yuki Hirai <yukihirai0505@gmail.com>
2018-04-27 20:01:38 +0900

    Add delete file randomly script

--- github/w-maker.js
+++ github/w-maker.js
@@ -2,22 +2,41 @@ var OWNER = 'yukihirai0505',
   REPO = 'github-w-maker',
   // GitHub access token from https://github.com/settings/tokens
   GITHUB_ACCESS_TOKEN = PropertiesService.getScriptProperties().getProperty('GITHUB_ACCESS_TOKEN'),
-  CREATE_FILE_URL = 'https://api.github.com/repos/' + OWNER + '/' + REPO + '/contents/:path?access_token=' + GITHUB_ACCESS_TOKEN;
+  GITHUB_BASE_API_URL = 'https://api.github.com',
+  REPOSITORY_CONTENTS_URL = GITHUB_BASE_API_URL + '/repos/' + OWNER + '/' + REPO + '/contents/' + '?access_token=' + GITHUB_ACCESS_TOKEN,
+  FILE_PATH_PLACEHOLDER = ':path',
+  FILE_URL = 'https://api.github.com/repos/' + OWNER + '/' + REPO + '/contents/' + FILE_PATH_PLACEHOLDER + '?access_token=' + GITHUB_ACCESS_TOKEN;
 
 // Create a file => https://developer.github.com/v3/repos/contents/#create-a-file
 function createFile() {
-  var message = 'w maker bot',
+  var message = 'create file randomly',
     params = {
       message: message,
       committer: {
-        name: 'GitHub w Maker bot',
-        email: 'example@ex.com'
+        name: 'yukihirai0505',
+        email: 'hogehoge@gmail.com'
       },
       content: Utilities.base64Encode(message)
     },
-    data = fetchJson(CREATE_FILE_URL.replace(':path', getRandomString()), 'PUT', params);
+    data = fetchJson(FILE_URL.replace(FILE_PATH_PLACEHOLDER, getRandomString()), 'PUT', params);
   Logger.log(data);
 }
+
+function deleteRandomFile() {
+  var files = fetchJson(REPOSITORY_CONTENTS_URL, 'GET'),
+    file = files[0],
+    params = {
+      message: 'delete file randomly',
+      committer: {
+        name: 'yukihirai0505',
+        email: 'hogehoge@gmail.com'
+      },
+      sha: file.sha
+    },
+    data = fetchJson(FILE_URL.replace(FILE_PATH_PLACEHOLDER, file.name), 'DELETE', params);
+  Logger.log(data);
+}
+
 
 function fetchJson(url, method, params) {
   var options = {
