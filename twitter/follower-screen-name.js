ad77023da9772d7436ab9d7ef15972e68e981d60
Yuki Hirai <yukihirai0505@gmail.com>
2018-05-14 10:02:43 +0900

    Update follower-screen-name.js

--- twitter/follower-screen-name.js
+++ twitter/follower-screen-name.js
@@ -1,37 +1,43 @@
 // Using TwitterWebService Project Key: 1rgo8rXsxi1DxI_5Xgo_t3irTw1Y5cxl2mGSkbozKsSXf2E_KBBPC3xTF
-var TWITTER_WEB_SERVICE = TwitterWebService.getInstance(
-  PropertiesService.getScriptProperties().getProperty('TWITTER_API_KEY'),
-  PropertiesService.getScriptProperties().getProperty('TWITTER_API_SECRET')
-  ).getService,
-  METHOD_TYPES = {
+var TWITTER_API_KEY = PropertiesService.getScriptProperties().getProperty('TWITTER_API_KEY'),
+  TWITTER_API_SECRET = PropertiesService.getScriptProperties().getProperty('TWITTER_API_SECRET');
+
+var twitterWebService = TwitterWebService.getInstance(
+  TWITTER_API_KEY,
+  TWITTER_API_SECRET
+);
+
+var METHOD_TYPES = {
     get: 'get',
     post: 'post'
-  };
+  },
+  BK = SpreadsheetApp.getActiveSpreadsheet(),
+  FOLLOWER_LIST_SHEET = BK.getSheetByName('FOLLOWER_LIST');
 
 // After execute authorize method, see log and copy and paste the url.
 // After call the url, you can authorize your app
 function authorize() {
-  TWITTER_WEB_SERVICE.authorize();
+  twitterWebService.authorize();
 }
 
 // Remove authorized app
 function reset() {
-  TWITTER_WEB_SERVICE.reset();
+  twitterWebService.reset();
 }
 
 // Callback for authorize
 function authCallback(request) {
-  return TWITTER_WEB_SERVICE.authCallback(request);
+  return twitterWebService.authCallback(request);
 }
 
 function fetchJson(url, method, params) {
   var response;
   if (method === METHOD_TYPES.get) {
-    response = TWITTER_WEB_SERVICE.fetch(url, {
+    response = twitterWebService.getService().fetch(url, {
       method: 'get'
     });
   } else if (method === METHOD_TYPES.post) {
-    response = TWITTER_WEB_SERVICE.fetch(url, {
+    response = twitterWebService.getService().fetch(url, {
       method: 'post',
       payload: params
     });
@@ -43,7 +49,7 @@ function fetchJson(url, method, params) {
 function usersLookup(userIds) {
   var url = 'https://api.twitter.com/1.1/users/lookup.json';
   var params = {
-    user_id: userIds.join(',')
+    user_id: userIds
   };
   return fetchJson(url, METHOD_TYPES.post, params);
 }
@@ -55,10 +61,19 @@ function followersIds(accountName) {
 }
 
 function test() {
-  followersIds('yabaiwebyasan').ids.eachSlice(100).forEach(function (userIds) {
-    Logger.log(userIds.length);
-    Logger.log(usersLookup(userIds));
-  });
+  var followerIds = followersIds('nomad_btc');
+  // keys => ids, next_cursor, next_cursor_str, previous_cursor, previous_cursor_str
+  var values = followerIds.ids.map(function (id) {
+    return id.toString();
+  }).eachSlice(100).map(function (userIds) {
+    var lookupData = usersLookup(userIds)
+      .map(function (account) {
+        return [account.screen_name];
+      });
+    Logger.log(lookupData.length);
+    return lookupData;
+  }).flatten();
+  FOLLOWER_LIST_SHEET.getRange(1, 1, values.length, 1).setValues(values)
 }
 
 Array.prototype.eachSlice = function (size) {
@@ -67,4 +82,13 @@ Array.prototype.eachSlice = function (size) {
     this.arr.push(this.slice(i, i + size))
   }
   return this.arr
+};
+
+Array.prototype.flatten = function () {
+  return this.reduce(
+    function (accumulator, currentValue) {
+      return accumulator.concat(currentValue);
+    },
+    []
+  );
 };
