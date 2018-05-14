ccf2d28bb5f1124cf2178b39e9d17771fe65de05
Yuki Hirai <yukihirai0505@gmail.com>
2018-05-14 10:55:50 +0900

    WIP

--- twitter/follower-screen-name.js
+++ twitter/follower-screen-name.js
@@ -12,7 +12,8 @@ var METHOD_TYPES = {
     post: 'post'
   },
   BK = SpreadsheetApp.getActiveSpreadsheet(),
-  FOLLOWER_LIST_SHEET = BK.getSheetByName('FOLLOWER_LIST');
+  FOLLOWERS_SHEET = BK.getSheetByName('FOLLOWERS'),
+  FRIENDS_SHEET = BK.getSheetByName('FRIENDS');
 
 // After execute authorize method, see log and copy and paste the url.
 // After call the url, you can authorize your app
@@ -47,22 +48,30 @@ function fetchJson(url, method, params) {
 
 // Get user account info by userIds
 function usersLookup(userIds) {
-  var url = 'https://api.twitter.com/1.1/users/lookup.json';
-  var params = {
-    user_id: userIds
-  };
-  return fetchJson(url, METHOD_TYPES.post, params);
+  try {
+    var url = 'https://api.twitter.com/1.1/users/lookup.json';
+    var params = {
+      user_id: userIds
+    };
+    return fetchJson(url, METHOD_TYPES.post, params);
+  } catch (err) {
+    return [{
+      screen_name: userIds + ': ' + err.message
+    }];
+  }
 }
 
-// Get user's follower ids
-function followersIds(accountName) {
-  var url = 'https://api.twitter.com/1.1/followers/ids.json?screen_name=' + accountName;
-  return fetchJson(url, METHOD_TYPES.get);
-}
+function getFollowers() {
+  // Get user's follower ids
+  function followersIds(screenName) {
+    var url = 'https://api.twitter.com/1.1/followers/ids.json?screen_name=' + screenName;
+    return fetchJson(url, METHOD_TYPES.get);
+  }
 
-function test() {
-  var followerIds = followersIds('nomad_btc');
+  var followerIds = followersIds('yabaiwebyasan');
+  FOLLOWER_LIST_SHEET.getRange(1, 1, values.length, 1).setValues(values);
   // keys => ids, next_cursor, next_cursor_str, previous_cursor, previous_cursor_str
+  /*
   var values = followerIds.ids.map(function (id) {
     return id.toString();
   }).eachSlice(100).map(function (userIds) {
@@ -74,6 +83,32 @@ function test() {
     return lookupData;
   }).flatten();
   FOLLOWER_LIST_SHEET.getRange(1, 1, values.length, 1).setValues(values)
+  */
+}
+
+function getFriends() {
+  function friendsIds(screenName) {
+    var url = 'https://api.twitter.com/1.1/friends/ids.json?screen_name=' + screenName;
+    return fetchJson(url, METHOD_TYPES.get);
+  }
+
+  var friends = friendsIds('yabaiwebyasan'),
+    ids = friends.ids.map(function (id) {
+      return [id];
+    });
+  FRIENDS_SHEET.getRange(1, 1, ids.length, 1).setValues(ids);
+  Logger.log(friends.ids.length);
+}
+
+function setScreenNames() {
+  var data = FRIENDS_SHEET.getRange(1, 1, FRIENDS_SHEET.getLastRow(), 1).getValues().map(function (id) {
+    return usersLookup(id)
+      .map(function (account) {
+        return [account.screen_name];
+      });
+  }).flatten();
+  Logger.log(data);
+  FRIENDS_SHEET.getRange(1, 2, data.length, 1).setValues(data);
 }
 
 Array.prototype.eachSlice = function (size) {
