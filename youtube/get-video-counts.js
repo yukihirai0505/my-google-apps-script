2fd575f484693a9ab23367a1a386272620ece3ad
Yuki Hirai <yukihirai0505@gmail.com>
2018-07-02 19:01:50 +0900

    Update get video counts

--- youtube/get-video-counts.js
+++ youtube/get-video-counts.js
@@ -51,6 +51,49 @@ function setYoutubeData() {
     });
   }
 
+  function getVideo(channelId) {
+    var videos = [],
+      videoIds = '',
+      videoList = [],
+      videosWithinMonth = [];
+
+    function getVideos(pageToken) {
+      var result = searchChannelVideos(channelId, pageToken),
+        _videos = result.items.filter(function (video) {
+          if (video.id.kind === 'youtube#video') {
+            return video;
+          }
+        }
+        ),
+        _videoIds = _videos.map(function (video) {
+          return video.id.videoId;
+        }).join(','),
+        _videoList = getVideoList(_videoIds).items,
+        _videosWithinMonth = filterVideoByDate(_videoList, ONE_MONTH_AGO);
+
+      videos.push.apply(videos, _videos);
+      videoIds += _videoIds;
+      videoList.push.apply(videoList, _videoList);
+      videosWithinMonth.push.apply(videosWithinMonth, _videosWithinMonth);
+
+      var nextPageToken = result.nextPageToken;
+      if (_videosWithinMonth.length > 35 && nextPageToken) {
+        getVideos(nextPageToken);
+      }
+    }
+
+    getVideos();
+    return {
+      title: videos[0].snippet.channelTitle,
+      withinMonth: videosWithinMonth.slice(0, 15),
+      beforeMonth: videoList.filter(function (video) {
+        if (new Date(video.snippet.publishedAt).getTime() <= ONE_MONTH_AGO.getTime()) {
+          return video;
+        }
+      }).slice(0, 15)
+    };
+  }
+
   var range = YOUTUBE_SHEET.getRange(2, 1, YOUTUBE_SHEET.getLastRow(), 9);
   var data = range.getValues().map(function (e, i) {
     var channelUrl = e[0],
@@ -67,40 +110,25 @@ function setYoutubeData() {
           channelId = getChannelId(userId).items[0].id
         }
         if (channelId) {
-          var videos = searchChannelVideos(channelId).items.filter(function (video) {
-              if (video.id.kind === 'youtube#video') {
-                return video;
-              }
-            }
-            ),
-            videoIds = videos.map(function (video) {
-              return video.id.videoId;
-            }).join(','),
-            videoList = getVideoList(videoIds).items,
-            videosWithinMonth = filterVideoByDate(videoList, ONE_MONTH_AGO).slice(0, 15),
-            videosBeforeMonth = videoList.filter(function (video) {
-              if (new Date(video.snippet.publishedAt).getTime() <= ONE_MONTH_AGO.getTime()) {
-                return video;
-              }
-            }).slice(0, 15);
-          e[1] = videos[0].snippet.channelTitle;
+          var video = getVideo(channelId);
+          e[1] = video.title;
           e[2] = channelId;
-          e[3] = average(videosWithinMonth.slice(0, 6), function (v) {
+          e[3] = average(video.withinMonth.slice(0, 6), function (v) {
             return v.statistics.viewCount;
           }, true);
-          e[4] = average(videosBeforeMonth, function (v) {
+          e[4] = average(video.beforeMonth, function (v) {
             return v.statistics.viewCount;
           }, true);
-          e[5] = average(videosWithinMonth, function (v) {
+          e[5] = average(video.withinMonth, function (v) {
             return v.statistics.likeCount;
           }, true);
-          e[6] = average(videosWithinMonth, function (v) {
+          e[6] = average(video.withinMonth, function (v) {
             return v.statistics.dislikeCount;
           }, true);
-          e[7] = average(videosWithinMonth, function (v) {
+          e[7] = average(video.withinMonth, function (v) {
             return v.statistics.commentCount;
           }, true);
-          e[8] = '=(F' + cellNum + '+H' + cellNum + ')/' + average(videosWithinMonth, function (v) {
+          e[8] = '=(F' + cellNum + '+H' + cellNum + ')/' + average(video.withinMonth, function (v) {
             return v.statistics.viewCount;
           }, false);
 
