76a62d35062984978fb0166b94f3bbea4f990eef
Yuki Hirai <yukihirai0505@gmail.com>
2018-08-21 18:19:21 +0900

    Add spreadsheet api

---
+++ spreadsheet/api.js
@@ -0,0 +1,23 @@
+var BK = SpreadsheetApp.getActiveSpreadsheet(),
+  SHEET = BK.getSheetByName('words');
+
+//  Spreadsheet API
+function doGet(e) {
+  var data = SHEET.getRange(1, 1, SHEET.getLastRow(), 4).getValues();
+  var keys = data[0];
+
+  data.shift();
+
+  var content = data.map(function (values) {
+    var obj = {};
+    values.forEach(function (value, index) {
+      obj[keys[index]] = value;
+    });
+    return obj;
+  });
+
+  var output = ContentService.createTextOutput();
+  output.setMimeType(ContentService.MimeType.JSON);
+  output.setContent(JSON.stringify(content));
+  return output;
+}
