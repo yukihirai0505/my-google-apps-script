7dddd78370dd243068b4730608785e70b9590668
Yuki Hirai <yukihirai0505@gmail.com>
2018-10-09 12:26:46 +0900

    Update tweet info js

--- twitter/tweet-info.js
+++ twitter/tweet-info.js
@@ -1,15 +1,19 @@
-var TWITTER_SHEET = bk.getSheetByName('twitter'),
-  twitterService = OAuth1.createService("Twitter")
-    .setAccessTokenUrl("https://api.twitter.com/oauth/access_token")
-    .setRequestTokenUrl("https://api.twitter.com/oauth/request_token")
-    .setAuthorizationUrl("https://api.twitter.com/oauth/authorize")
-    .setConsumerKey(getScriptProperty('TWITTER_COMSUMER_KEY'))
-    .setConsumerSecret(getScriptProperty('TWITTER_COMSUMER_SECRET'))
-    .setAccessToken(getScriptProperty('TWITTER_ACCESS_TOKEN'), getScriptProperty('TWITTER_ACCESS_SECRET'));
+var TWITTER_SHEET = bk.getSheetByName('twitter');
 
+// OAuth1 Library => 1CXDCY5sqT9ph64fFwSzVtXnbjpSfWdRymafDrtIZ7Z_hwysTY7IIhi7s
 function setTweetsInfo() {
+  function getTwitterService() {
+    return OAuth1.createService("Twitter")
+      .setAccessTokenUrl("https://api.twitter.com/oauth/access_token")
+      .setRequestTokenUrl("https://api.twitter.com/oauth/request_token")
+      .setAuthorizationUrl("https://api.twitter.com/oauth/authorize")
+      .setConsumerKey(getScriptProperty('TWITTER_COMSUMER_KEY'))
+      .setConsumerSecret(getScriptProperty('TWITTER_COMSUMER_SECRET'))
+      .setAccessToken(getScriptProperty('TWITTER_ACCESS_TOKEN'), getScriptProperty('TWITTER_ACCESS_SECRET'));
+  }
+
   function getTweetInfo(id) {
-    var res = twitterService.fetch("https://api.twitter.com/1.1/statuses/show.json?id=" + id);
+    var res = getTwitterService().fetch("https://api.twitter.com/1.1/statuses/show.json?id=" + id);
     return JSON.parse(res);
   }
 
