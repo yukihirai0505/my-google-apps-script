4b89cfd6008eaab3533b7ca70c42a8885ba8ec8e
Yuki Hirai <yukihirai0505@gmail.com>
2018-10-17 18:39:50 +0900

    Update simple api count

--- simpleApi/count.js
+++ simpleApi/count.js
@@ -1,23 +1,30 @@
 var COUNT_KEY = 'count';
 
+function doPost(e) {
+  return createResponse(countUp());
+}
+
 function doGet(e) {
-  function getCount() {
-    return Number(PropertiesService.getScriptProperties().getProperty(COUNT_KEY));
-  }
+  return createResponse(getCount());
+}
 
-  function countUp() {
-    var count = getCount() + 1;
-    PropertiesService.getScriptProperties().setProperty(COUNT_KEY, count);
-    return count;
-  }
-
-  var isCountUp = e.parameter.is_count_up,
-    output = ContentService.createTextOutput();
+function createResponse(count) {
+  var output = ContentService.createTextOutput();
   output.setMimeType(ContentService.MimeType.JSON);
   output.setContent(
     JSON.stringify({
-      count: isCountUp ? countUp() : getCount()
+      count: count
     })
   );
   return output;
+}
+
+function getCount() {
+  return Number(PropertiesService.getScriptProperties().getProperty(COUNT_KEY));
+}
+
+function countUp() {
+  var count = getCount() + 1;
+  PropertiesService.getScriptProperties().setProperty(COUNT_KEY, count);
+  return count;
 }
