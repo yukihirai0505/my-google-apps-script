d24b2cdcb7b3f1020668f578d1f2329ee2bdffd9
Yuki Hirai <yukihirai0505@gmail.com>
2018-10-17 15:37:40 +0900

    Remove clasp sample

--- clasp-sample/src/index.ts
+++
@@ -1,8 +0,0 @@
-import { SheetService } from './sheet.service';
-
-declare var global: any;
-
-global.createNewFile = (): void => {
-  const ss = SheetService.createInitialFile('New file');
-  ss.getRange('A2').setValue('Happy gas!');
-};
