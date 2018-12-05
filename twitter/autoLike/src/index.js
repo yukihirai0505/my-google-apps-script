67aaa2d6ab8b2a41f3e930d3eb52973dbd845a6f
Yuki Hirai <yukihirai0505@gmail.com>
2018-12-05 10:06:24 +0900

    Refactoring twitter autoLike

--- twitter/autoLike/src/index.js
+++ twitter/autoLike/src/index.js
@@ -1,5 +1,36 @@
-import { autoLike } from './twitter'
+import { favorite, search, show } from './twitter'
+
+const getRandomInt = (min, max) => Math.floor(Math.random() * (max - min + 1) + min)
 
 global.autoLike = () => {
-  autoLike()
+  const ids = []
+  const keywords = [
+    '#Progate',
+    '#Dotinstall',
+    '#テックキャンプ',
+    '#Qiita',
+    '#駆け出しエンジニアと繋がりたい',
+    '#Railsチュートリアル'
+  ]
+  keywords.forEach(keyword => {
+    const { statuses: tweets } = search(keyword)
+    tweets.forEach(tweet => {
+      // 1000/24 hour
+      if (ids.length > 40) {
+        return
+      }
+      const diff = Math.abs(new Date() - new Date(tweet.created_at))
+      const minutes = Math.floor(diff / 1000 / 60)
+      if (minutes < 60) {
+        const { id_str: idStr } = tweet
+        // search result tweet.favorited is not correct somthimes, so fetch it again
+        const status = show(idStr)
+        if (ids.filter(e => e === idStr).length === 0 && status.favorited === false) {
+          favorite(idStr)
+          Utilities.sleep(getRandomInt(1000, 2000))
+          ids.push(idStr)
+        }
+      }
+    })
+  })
 }
--- twitter/autoLike/src/twitter.js
+++ twitter/autoLike/src/twitter.js
@@ -9,49 +9,21 @@ const getTwitterService = () =>
     .setConsumerSecret(getProperty('TWITTER_COMSUMER_SECRET'))
     .setAccessToken(getProperty('TWITTER_ACCESS_TOKEN'), getProperty('TWITTER_ACCESS_SECRET'))
 
-const keywords = [
-  '#Progate',
-  '#Dotinstall',
-  '#テックキャンプ',
-  '#Qiita',
-  '#駆け出しエンジニアと繋がりたい',
-  '#Railsチュートリアル'
-]
 const twitterService = getTwitterService()
 
-const getRandomInt = (min, max) => Math.floor(Math.random() * (max - min + 1) + min)
-
-export const autoLike = () => {
-  keywords.forEach(keyword => {
-    const res = twitterService.fetch(
+export const search = keyword =>
+  JSON.parse(
+    twitterService.fetch(
       `https://api.twitter.com/1.1/search/tweets.json?q=${encodeURIComponent(
         `${keyword} exclude:retweets`
       )}&lang=ja&result_type=recent&count=100`
     )
-    const { statuses: tweets } = JSON.parse(res)
-    const ids = []
-    tweets.forEach(tweet => {
-      // 1000/24 hour
-      if (ids.length > 40) {
-        return
-      }
+  )
 
-      const diff = Math.abs(new Date() - new Date(tweet.created_at))
-      const minutes = Math.floor(diff / 1000 / 60)
-      if (minutes < 60) {
-        const { id_str: idStr } = tweet
-        // search result tweet.favorited is not correct somthimes, so fetch it again
-        const status = JSON.parse(
-          twitterService.fetch(`https://api.twitter.com/1.1/statuses/show.json?id=${idStr}`)
-        )
-        if (ids.filter(e => e === idStr).length === 0 && status.favorited === false) {
-          twitterService.fetch(`https://api.twitter.com/1.1/favorites/create.json?id=${idStr}`, {
-            method: 'post'
-          })
-          Utilities.sleep(getRandomInt(1000, 2000))
-          ids.push(idStr)
-        }
-      }
-    })
+export const show = idStr =>
+  JSON.parse(twitterService.fetch(`https://api.twitter.com/1.1/statuses/show.json?id=${idStr}`))
+
+export const favorite = idStr =>
+  twitterService.fetch(`https://api.twitter.com/1.1/favorites/create.json?id=${idStr}`, {
+    method: 'post'
   })
-}
