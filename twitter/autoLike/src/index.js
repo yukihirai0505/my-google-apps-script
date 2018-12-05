68d946f4ede08de355ab62989002639bc1c33afa
Yuki Hirai <yukihirai0505@gmail.com>
2018-12-05 09:55:46 +0900

    Refactoring autoLike

--- twitter/autoLike/src/index.js
+++ twitter/autoLike/src/index.js
@@ -1,5 +1,35 @@
-import { autoLike } from './twitter'
+import { favorite, search } from './twitter'
 
 global.autoLike = () => {
-  autoLike()
+  const urls = []
+  const keywords = [
+    '#Progate',
+    '#Dotinstall',
+    '#テックキャンプ',
+    '#Qiita',
+    '#駆け出しエンジニアと繋がりたい',
+    '#Railsチュートリアル'
+  ]
+
+  keywords.forEach(keyword => {
+    const res = search(keyword)
+    const { statuses: tweets } = JSON.parse(res)
+
+    tweets.forEach(tweet => {
+      // 1000/24 hour ref: https://developer.twitter.com/en/docs/basics/rate-limits.html
+      if (urls.length > 40) {
+        return
+      }
+
+      const diff = Math.abs(new Date() - new Date(tweet.created_at))
+      const minutes = Math.floor(diff / 1000 / 60)
+      if (minutes < 60) {
+        const url = `https://twitter.com/${tweet.user.screen_name}/status/${tweet.id_str}`
+        if (urls.filter(e => e === url).length === 0) {
+          urls.push(url)
+          favorite(tweet.id_str)
+        }
+      }
+    })
+  })
 }
--- twitter/autoLike/src/twitter.js
+++ twitter/autoLike/src/twitter.js
@@ -9,44 +9,16 @@ const getTwitterService = () =>
     .setConsumerSecret(getProperty('TWITTER_COMSUMER_SECRET'))
     .setAccessToken(getProperty('TWITTER_ACCESS_TOKEN'), getProperty('TWITTER_ACCESS_SECRET'))
 
-const urls = []
-const keywords = [
-  '#Progate',
-  '#Dotinstall',
-  '#テックキャンプ',
-  '#Qiita',
-  '#駆け出しエンジニアと繋がりたい',
-  '#Railsチュートリアル'
-]
 const twitterService = getTwitterService()
 
-export const autoLike = () => {
-  keywords.forEach(keyword => {
-    const res = twitterService.fetch(
-      `https://api.twitter.com/1.1/search/tweets.json?q=${encodeURIComponent(
-        `${keyword} exclude:retweets`
-      )}&lang=ja&result_type=recent&count=100`
-    )
-    const { statuses: tweets } = JSON.parse(res)
+export const search = keyword =>
+  twitterService.fetch(
+    `https://api.twitter.com/1.1/search/tweets.json?q=${encodeURIComponent(
+      `${keyword} exclude:retweets`
+    )}&lang=ja&result_type=recent&count=100`
+  )
 
-    tweets.forEach(tweet => {
-      // 1000/24 hour
-      if (urls.length > 40) {
-        return
-      }
-
-      const diff = Math.abs(new Date() - new Date(tweet.created_at))
-      const minutes = Math.floor(diff / 1000 / 60)
-      if (minutes < 60) {
-        const url = `https://twitter.com/${tweet.user.screen_name}/status/${tweet.id_str}`
-        if (urls.filter(e => e === url).length === 0) {
-          urls.push(url)
-          twitterService.fetch(
-            `https://api.twitter.com/1.1/favorites/create.json?id=${tweet.id_str}`,
-            { method: 'post' }
-          )
-        }
-      }
-    })
+export const favorite = idStr =>
+  twitterService.fetch(`https://api.twitter.com/1.1/favorites/create.json?id=${idStr}`, {
+    method: 'post'
   })
-}
